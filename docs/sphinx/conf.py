9c0a681dbb649bf6da5cb3333f7ba232dc6b9148
Mathias Giacomuzzi <ecam@gmx.at>
2018-05-07 16:51:15 +0200

    rtd next try

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import subprocess
+import shutil
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -336,8 +337,10 @@ def run_invoke():
 
 
 def generate_doxygen_xml(app):
+    print("generate doxy...")
     if on_rtd:
-        call(['doxygen', './../../build/docs/doxy/li_can_slv.doxyfile'])
+        subprocess.call(['doxygen', './../doxy/li_can_slv.doxyfile'])
+        shutil.copytree('./../../build/docs/doxy/xml', '_doxy/xml')
     else:
         run_invoke()
 
