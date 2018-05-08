64a264a5264d05c927417a2a7f9075c062f6f747
Mathias Giacomuzzi <ecam@gmx.at>
2018-05-08 11:46:25 +0200

    fix theme

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -148,12 +148,11 @@ todo_include_todos = False
 # html_theme = 'alabaster'
 # html_theme = 'bootstrap'
 # html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-if on_rtd:
-    html_theme = 'default'
-else:
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_path = ['.', sphinx_rtd_theme.get_html_theme_path()]
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
