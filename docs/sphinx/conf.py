9cfb5e178a29bf40750ee7e8638c051c9459e56b
Mathias Giacomuzzi <ecam@gmx.at>
2018-05-08 13:31:58 +0200

    next try for doc

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -182,7 +182,7 @@ else:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_doxy', '_doxy/xml']
+html_static_path = ['.//_static', './/_doxy', './/_doxy/xml']
 
 # html_context = {
 #     'css_files': ['_static/custom.css'],
