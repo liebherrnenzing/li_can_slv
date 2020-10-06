bb158e8dc211bc86e63aedbb497d085aac57b936
Mathias Giacomuzzi <ecam@gmx.at>
2020-10-06 12:56:37 +0200

    ignore 29Bit messages on stack rx handler

--- source/arch/canpie_adapter/io_can_main_hw_handler.c
+++ source/arch/canpie_adapter/io_can_main_hw_handler.c
@@ -132,6 +132,12 @@ uint8_t can_main_hw_handler_rx(CpCanMsg_ts *ptsCanMsgV, uint8_t ubBufferIdxV)
 	}
 #endif // #ifdef LI_CAN_SLV_RECONNECT
 
+	// 29Bit is not handled here
+	if(CpMsgIsExtended(ptsCanMsgV))
+	{
+		return (LI_CAN_SLV_ERR_OK);
+	}
+
 	canid = CpMsgGetStdId(ptsCanMsgV);
 	CpCoreBufferGetDlc(&can_port_main, ubBufferIdxV, &dlc);
 #if CP_VERSION_MAJOR <= 2
