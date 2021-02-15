bfbbf53a65067cec13ab9e9198338d1a4cfee94b
Mathias Giacomuzzi <ecam@gmx.at>
2021-02-15 15:27:35 +0100

    update req.txt

--- include/li_can_slv_version.h
+++ include/li_can_slv_version.h
@@ -2,4 +2,4 @@
 #pragma once
 
 // The output from git --describe (e.g. the most recent tag)
-#define LI_CAN_SLV_VERSION_STRING "v1.0.1-46-g9057adc"
+#define LI_CAN_SLV_VERSION_STRING "v1.0.1-48-g413eef1"
